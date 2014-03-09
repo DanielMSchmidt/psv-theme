52ae812dd8c18a70fd9ff1a3f488765435b293ea
Daniel Schmidt <dschmidt@weluse.de>
2014-03-09 16:28:07 +0100

    removed footer part

--- themes/twentyfourteen/footer.php
+++ themes/twentyfourteen/footer.php
@@ -12,11 +12,6 @@
 	</div><!-- #main -->
 	<div id="separator" class="clearfix"> </div>
 
-  <div id="site-info" class="clearfix">
-    <span class="copyright">Copyright <?= date(Y); ?> <?php bloginfo( 'name' ); ?></span>
-    </a>
-  </div><!-- #site-info -->
-
 </div><!-- #wrapper -->
 
 <?php wp_footer(); ?>
